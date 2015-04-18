4784045a624c62b24ec8f53f00b5f4444aebfc4c
Pedro Silva <pedro.cls93@gmail.com>
2015-04-18 22:04:53 +0100

    UI v1.0.1

--- Client/Client.cs
+++ Client/Client.cs
@@ -87,6 +87,16 @@ namespace Client
                     
                     break;
                 case Operation.Remove:
+                    int i=0;
+                    foreach (ListViewItem item in itemListView.Items)
+                    {
+                        if (order.Id.ToString().Equals(item.Text))
+                        {
+                            i = itemListView.Items.IndexOf(item);
+                            break;
+                        }
+                    }
+                    itemListView.Items.RemoveAt(i);
 
                     break;
                 case Operation.Notify:
@@ -124,35 +134,10 @@ namespace Client
                 MessageBox.Show("Hello World! Here is the output of the register action: " + result);
                 api.GetUserByName("NONEEE");
                 api.GetUserByName(textBox2.Text);
-
-
-                //api.removeUserByName(textBox2.Text);
-
-                /*api.RegisterDiginote(aux);
-                api.RegisterDiginote(aux);
-                api.RegisterDiginote(aux);
-                api.RegisterDiginote(aux);*/
-
                 DOrder tempOrder = new DOrder(aux, 5, 5.0, OrderType.Buy, DateTime.Now);
                 api.RegisterOrder(ref tempOrder);
-                /*api.DeleteOrder(tempOrder);
-                api.RegisterOrder(ref tempOrder);*/
-
-
-                /*DTransaction tempTrans = new DTransaction(aux, 5.0, tempOrder);
-                api.RegisterTransaction(tempTrans);
-                api.DeleteTransaction(tempTrans);
-                api.RegisterTransaction(tempTrans);
-
-                api.GetOrder(tempOrder.Id);
-                api.GetTransaction(tempTrans.Order.Id);*/
-
                 tempOrder.Amount = 1333;
                 api.EditOrder(tempOrder);
-
-                //Só se vai buscar as orders no login bem sucedido
-                //orders = api.ActiveOrders;
-                //MessageBox.Show("Number of orders:"+orders.Count);
             }
             catch (Exception error)
             {
@@ -169,7 +154,6 @@ namespace Client
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //login user
             if ((userSession = api.ValidateUser(textBox4.Text, textBox5.Text)) != null)
             {
                 UserLbl.Text = userSession.Nickname;
@@ -189,7 +173,6 @@ namespace Client
             loginPanel.Visible = false;
             registerPanel.Visible = false;
             ExchangePanel.Visible = true;
-            //IAPI.getActiveTransactions();
         }
 
         private void showInitialPanel()
