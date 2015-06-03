bd95eb89018ec8803ce852d6a4f85224077eb296
Guntler <pbpdirector@hotmail.com>
2015-06-03 17:21:31 +0100

    store gui stuff

---
+++ TDIN-2/Enterprise distributed system/StoreApp/Landing.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StoreApp
+{
+    /// <summary>
+    /// Interaction logic for Landing.xaml
+    /// </summary>
+    public partial class Landing : Window
+    {
+        public Landing()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
